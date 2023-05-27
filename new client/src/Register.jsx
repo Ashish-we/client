52107de857189cf3c0f6297ab8a067216859e348
Ashish-we <acharya.seven@gmail.com>
2023-05-27 17:36:47 +0545

    hello

---
+++ new client/src/Register.jsx
@@ -0,0 +1,3 @@
+const Registerapi ='http://127.0.0.1:8000/api/register';
+
+export default Registerapi;
---
+++ new client/src/Request.jsx
@@ -0,0 +1,8 @@
+
+const Loginapi = 'http://127.0.0.1:8000/api/login';
+
+//  Loginapi Registerapi;
+
+
+
+export default Loginapi;
